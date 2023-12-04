8298bba9e8d75638e31accd69b9a0c853708e155
Bevan Hunt <bevan@bevanhunt.com>
2023-12-03 19:58:38 -0800

    first

---
+++ src/stablethread/ai.py
@@ -0,0 +1,5 @@
+from stablethread.components.ai_classifier import ai_classifier as classifier
+from stablethread.components.ai_function import ai_fn as fn
+from stablethread.components.ai_model import ai_model as model
+
+__all__ = ["fn", "model", "classifier"]
