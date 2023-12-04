17ef44e649f4a95eac510f36417f2197f73fcc73
Bevan Hunt <bevan@bevanhunt.com>
2023-12-04 15:50:19 -0800

    fix prompt init

--- cyberchipped/components/__init__.py
+++ cyberchipped/components/__init__.py
@@ -2,7 +2,7 @@ from regex import B
 from .ai_function import ai_fn, AIFunction
 from .ai_classifier import ai_classifier, AIClassifier
 from .ai_model import ai_model
-from .prompt import Prompt
+from .prompt import prompt_fn, PromptFunction
 
 __all__ = [
     "ai_fn",
