c384256cb0703f00b4068dc8092a827f1d3cc59c
Bevan Hunt <bevan@bevanhunt.com>
2024-01-07 02:24:47 -0800

    v0.6.1 - do not specify any language for listen

--- cyberchipped/components/ai_listen.py
+++ cyberchipped/components/ai_listen.py
@@ -4,13 +4,11 @@ from openai._types import FileTypes
 
 async def ai_listen(
     file: FileTypes,
-    language: str = "en",
     response_format: str = "text",
 ):
     client = get_client()
     response = await client.audio.transcriptions.create(
         file=file,
-        language=language,
         model="whisper-1",
         response_format=response_format,
     )
